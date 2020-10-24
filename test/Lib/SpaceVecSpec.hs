12887196a1d03c8f13251bb576d550df5b623783
Robert Brennan <contact@rbren.io>
2020-10-24 19:12:06 +0000

    more tests

--- test/Lib/SpaceVecSpec.hs
+++ test/Lib/SpaceVecSpec.hs
@@ -19,3 +19,24 @@ spec = do
       zeroSV ^+^ v1 `shouldBe` v1
       v1 ^+^ zeroSV `shouldBe` v1
       v1 ^+^ v1 `shouldBe` SpaceVec 2 4 6
+
+    it "should dot product" $ do
+      zeroSV ^.^ zeroSV `shouldBe` 0.0
+
+      let v1 = SpaceVec 1 2 3
+      (zeroSV ^.^ v1) `shouldBe` (v1 ^.^ zeroSV)
+      (zeroSV ^.^ v1) `shouldBe` 0.0
+      (v1 ^.^ v1) `shouldBe` 14.0
+
+    it "should have scalar multiplication" $ do
+      zeroSV ^* 2.0 `shouldBe` zeroSV
+      2.0 *^ zeroSV `shouldBe` zeroSV
+
+      let v1 = SpaceVec 1 2 3
+      v1 ^* 2.0 `shouldBe` SpaceVec 2 4 6
+      2.0 *^ v1 `shouldBe` SpaceVec 2 4 6
+      v1 ^* 0.0 `shouldBe` SpaceVec 0 0 0
+      0.0 *^ v1 `shouldBe` SpaceVec 0 0 0
+
+    it "should have magnitude" $ do
+      magnitude zeroSV `shouldBe` 0.0

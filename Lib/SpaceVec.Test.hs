0c2d40e2ab2948dcb1890c9e92d76569d3231bef
Robert Brennan <contact@rbren.io>
2020-10-24 18:53:24 +0000

    add basic tests

---
+++ Lib/SpaceVec.Test.hs
@@ -0,0 +1,19 @@
+import Test.Hspec
+import Test.QuickCheck
+import Lib.SpaceVec
+
+main :: IO ()
+main = hspec $ do
+  describe "SpaceVec" $ do
+    it "has zero vec" $ do
+      x zeroSV `shouldBe` 0.0
+      y zeroSV `shouldBe` 0.0
+      z zeroSV `shouldBe` 0.0
+
+    it "should add properly" $ do
+      zeroSV ^+^ zeroSV `shouldBe` zeroSV
+
+      let v1 = SpaceVec 1 2 3
+      zeroSV ^+^ v1 `shouldBe` v1
+      v1 ^+^ zeroSV `shouldBe` v1
+      v1 ^+^ v1 `shouldBe` SpaceVec 2 4 6
--- Lib/SpaceVec.hs
+++ Lib/SpaceVec.hs
@@ -9,6 +9,9 @@ data SpaceVec = SpaceVec {
 instance Show SpaceVec where
   show (SpaceVec x y z) = "<" ++ show x ++ "," ++ show y ++ "," ++ show z ++ ">"
 
+instance Eq SpaceVec where
+  (==) (SpaceVec x y z) (SpaceVec x' y' z') = x == x' && y == y' && z == z'
+
 (^+^) :: SpaceVec -> SpaceVec -> SpaceVec
 (^+^) (SpaceVec x y z) (SpaceVec x' y' z') = SpaceVec (x+x') (y+y') (z+z')
 
@@ -21,3 +24,10 @@ instance Show SpaceVec where
 (^*) :: SpaceVec -> Double -> SpaceVec
 (^*) (SpaceVec x y z) c = SpaceVec (c*x) (c*y) (c*z)
 
+(^.^) :: SpaceVec -> SpaceVec -> Double
+(^.^) (SpaceVec x y z) (SpaceVec x' y' z') = x*x' + y*y' + z*z'
+
+magnitude :: SpaceVec -> Double
+magnitude (SpaceVec x y z) = sqrt (x*x + y*y + z*z)
+
+zeroSV = SpaceVec 0 0 0
